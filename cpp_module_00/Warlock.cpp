f63509fb0e427de9443a78125f262ffff494728b
Lucas Lethuillier <llethuil@student.42lyon.fr>
2022-12-08 10:50:14 +0100

    implementation(Warlock): private default constructor and private copy constructor implemented in Warlock.cpp file of cpp_module_00 | organization(code): comments added in .hpp and .cpp files for more clarity

--- cpp_module_00/Warlock.cpp
+++ cpp_module_00/Warlock.cpp
@@ -1,5 +1,6 @@
 #include "Warlock.hpp"
 
+// CONSTRUCTOR
 Warlock::Warlock(std::string name, std::string title):
 	name(name), title(title)
 {
@@ -7,12 +8,14 @@ Warlock::Warlock(std::string name, std::string title):
 	return ;
 }
 
+// DESTRUCTOR
 Warlock::~Warlock(void)
 {
 	std::cout << this->name << ": My job here is done!" << std::endl;
 	return ;
 }
 
+// GETTERS
 std::string	Warlock::getName(void) const
 {
 	return (this->name);
@@ -23,14 +26,37 @@ std::string	Warlock::getTitle(void) const
 	return (this->title);
 }
 
+// SETTER
 void	Warlock::setTitle(const std::string& title)
 {
 	this->title = title;
 	return;
 }
 
+// METHOD
 void	Warlock::introduce(void) const
 {
 	std::cout << this->name << ": I am " << this->name << ", " << title << std::endl;
 	return ;
+}
+
+// PRIVATE DEFAULT CONSTRUCTOR
+Warlock::Warlock(void)
+{
+	return ;
+}
+
+// PRIVATE COPY CONSTRUCTOR
+Warlock::Warlock(Warlock const & src)
+{
+	*this = src;
+	return ;
+}
+
+// PRIVATE '=' OPERATOR OVERLAOD
+Warlock&	Warlock::operator=(Warlock const & src)
+{
+	this->name = src.name;
+	this->title = src.title;
+	return (*this);
 }
--- cpp_module_00/Warlock.hpp
+++ cpp_module_00/Warlock.hpp
@@ -7,19 +7,26 @@
 class Warlock
 {
 	public:
-
+		// CONSTRUCTOR
 					Warlock(std::string name, std::string title);
+		// DESTRUCTOR
 					~Warlock(void);
+		// GETTERS
 		std::string	getName(void)	const;
 		std::string	getTitle(void)	const;
+		// SETTERS
 		void		setTitle(const std::string& title);
+		// METHOD
 		void		introduce(void)	const;
 
 	private:
-
+		// PRIVATE DEFAULT CONSTRUCTOR
 					Warlock(void);
+		// PRIVATE COPY CONSTRUCTOR
 					Warlock(Warlock const & src);
+		// PRIVATE '=' OPERATOR OVERLAOD
 		Warlock&	operator=(Warlock const & src);
+		// PRIVATE ATTRIBUTES
 		std::string	name;
 		std::string	title;
 };
