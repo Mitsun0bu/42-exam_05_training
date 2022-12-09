80333b31baad83626035603fb76ccd684c487e7b
Lucas Lethuillier <llethuil@student.42lyon.fr>
2022-12-09 11:52:26 +0100

    implementation(Warlock): Warlock.cpp/.hpp implementation done

---
+++ cpp_module_01/Warlock.cpp
@@ -0,0 +1,89 @@
+#include "Warlock.hpp"
+
+// CONSTRUCTOR
+Warlock::Warlock(std::string name, std::string title):
+	name(name), title(title)
+{
+	std::cout
+	<< this->name << ": This looks like another boring day."
+	<< std::endl;
+	return ;
+}
+
+// DESTRUCTOR
+Warlock::~Warlock(void)
+{
+	std::cout
+	<< this->name << ": My job here is done!"
+	<< std::endl;
+	return ;
+}
+
+// GETTERS
+std::string	Warlock::getName(void) const
+{
+	return (this->name);
+}
+
+std::string	Warlock::getTitle(void) const
+{
+	return (this->title);
+}
+
+// SETTER
+void	Warlock::setTitle(const std::string& title)
+{
+	this->title = title;
+	return;
+}
+
+// METHODS
+void	Warlock::introduce(void) const
+{
+	std::cout
+	<< this->name << ": I am " << this->name
+	<< ", " << title
+	<< std::endl;
+	return ;
+}
+
+void	Warlock::learnSpell(ASpell* spell)
+{
+	if (!spell)
+		return;
+	this->spellList[spell->getName()] = spell;
+	return ;
+}
+
+void	Warlock::forgetSpell(std::string spellName)
+{
+	spellList.erase(spellName);
+	return ;
+}
+
+void	Warlock::launchSpell(std::string spellName, ATarget& target)
+{
+	target.getHitBySpell(*spellList[spellName]);
+	return ;
+}
+
+// PRIVATE DEFAULT CONSTRUCTOR
+Warlock::Warlock(void)
+{
+	return ;
+}
+
+// PRIVATE COPY CONSTRUCTOR
+Warlock::Warlock(Warlock const & src)
+{
+	*this = src;
+	return ;
+}
+
+// PRIVATE '=' OPERATOR OVERLAOD
+Warlock&	Warlock::operator=(Warlock const & src)
+{
+	this->name = src.name;
+	this->title = src.title;
+	return (*this);
+}
---
+++ cpp_module_01/Warlock.hpp
@@ -0,0 +1,46 @@
+# ifndef WARLOCK_HPP
+# define WARLOCK_HPP
+
+# include <iostream>
+# include <string>
+# include <map>
+# include <iterator>
+# include "ASpell.hpp"
+# include "ATarget.hpp"
+
+
+class ASpell;
+class ATarget;
+
+class Warlock
+{
+	public:
+		// CONSTRUCTOR
+					Warlock(std::string name, std::string title);
+		// DESTRUCTOR
+					~Warlock(void);
+		// GETTERS
+		std::string	getName(void) const;
+		std::string	getTitle(void) const;
+		// SETTERS
+		void		setTitle(const std::string& title);
+		// METHODS
+		void		introduce(void)	const;
+		void		learnSpell(ASpell* spell);
+		void		forgetSpell(std::string spellName);
+		void		launchSpell(std::string spellName, ATarget& target);
+		std::map<std::string, ASpell*>	spellList;
+
+	private:
+		// PRIVATE DEFAULT CONSTRUCTOR
+					Warlock(void);
+		// PRIVATE COPY CONSTRUCTOR
+					Warlock(Warlock const & src);
+		// PRIVATE '=' OPERATOR OVERLAOD
+		Warlock&	operator=(Warlock const & src);
+		// PRIVATE ATTRIBUTES
+		std::string						name;
+		std::string						title;
+};
+
+# endif
